6c95e9ee0d3ff2f2df0374e75e471381a79e2a88
Jarryd Long <info@jarrydlong.co.za>
2016-09-08 11:54:14 +0200

    Mockup Expectation

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -5,9 +5,14 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	use PHPMock;
 
 	public function test_wcct_get_option(){
-		$this->assertEquals('river-club', wcct_get_option('venue'));
 
 		$get_option = $this->getFunctionMock('WCCT', 'get_option');
+
+		$get_option->expects($this->once())->with($this->equalTo('wcct_venue'))->willReturn('river-club')
+
+		$this->assertEquals('river-club', wcct_get_option('venue'));
+
+
 	}
 
 }
